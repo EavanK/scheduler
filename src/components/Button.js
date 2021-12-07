8b08c7b3ddf8953e82d194aa6187252ae1e52232
Hyunsu <ksoksl0711@gmail.com>
2021-12-07 14:24:54 -0800

    modify Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,24 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const {confirm, danger, onClick, disabled, children} = props;
+   let buttonClass = "button";
+
+   if (confirm) {
+      buttonClass += " button--confirm";
+   }
+
+   if (danger) {
+      buttonClass += " button--danger";
+   }
+
+   return (
+      <button
+         className={buttonClass}
+         onClick={onClick}
+         disabled={disabled}
+      >
+      {children}
+      </button>
+   );
 }
