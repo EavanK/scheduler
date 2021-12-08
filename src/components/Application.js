91ec3599145ffd84672152bbd036cbd3fdd44f94
Hyunsu <ksoksl0711@gmail.com>
2021-12-07 21:00:42 -0800

    render DayList component, add userSate to pass data down via props

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import DayList from "./DayList";
 
 import "components/Application.scss";
 
+
 export default function Application(props) {
+  const days = [
+    { id: 1,
+      name: "Monday",
+      spots: 2,},
+    { id: 2,
+      name: "Tuesday",
+      spots: 5,},
+    { id: 3,
+      name: "Wednesday",
+      spots: 0,},
+  ];
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +28,9 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
