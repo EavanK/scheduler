440230cc6c2890a730367548fad6ed7d00d6d45e
Hyunsu <ksoksl0711@gmail.com>
2021-12-08 22:06:59 -0800

    add props.id

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,21 +3,68 @@ import DayList from "./DayList";
 
 import "components/Application.scss";
 
+import Appointment from "components/Appointment";
+
+//mock appointments data
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+// mock days data
+const days = [
+  { id: 1,
+    name: "Monday",
+    spots: 2,},
+  { id: 2,
+    name: "Tuesday",
+    spots: 5,},
+  { id: 3,
+    name: "Wednesday",
+    spots: 0,},
+];
 
 export default function Application(props) {
-  const days = [
-    { id: 1,
-      name: "Monday",
-      spots: 2,},
-    { id: 2,
-      name: "Tuesday",
-      spots: 5,},
-    { id: 3,
-      name: "Wednesday",
-      spots: 0,},
-  ];
 
   const [day, setDay] = useState("Monday");
+  const appointmentListItem = appointments.map(appointment => {
+    return (
+      <Appointment key={appointment.id} {...appointment} />
+    );
+  });
 
   return (
     <main className="layout">
@@ -38,7 +85,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentListItem}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
