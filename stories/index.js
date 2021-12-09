985a91290de4e34b7f25cf006091371c4ad74933
Hyunsu <ksoksl0711@gmail.com>
2021-12-08 18:03:24 -0800

    add Confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 //Button componenet
@@ -158,9 +159,16 @@ storiesOf("Appointment", module)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => (
     <Show 
-    student="Lydia Miller-Jones"
-    interviewer={interviewer}
-    onEdit={action("onEdit")}
-    onDelete={action("onDelete")}
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
     />
   ))
