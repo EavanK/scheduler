51da9dbd0c96e069a9293a2a4d299991c216cfa1
Hyunsu <ksoksl0711@gmail.com>
2021-12-08 21:39:28 -0800

    import Header, Show, Empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-  const { time } = props;
-  const formatTime = time => time ? `Appointment at ${time}` : `No Appointment`;
+  const { id, time, interview } = props;
 
   return (  
-    <article className="appointment">{formatTime(time)}</article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
