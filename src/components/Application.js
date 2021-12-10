f1136fd13159326b6f6c71eb4c2137abdda71994
Hyunsu <ksoksl0711@gmail.com>
2021-12-09 20:03:42 -0800

    combine day, days states to one "state", modify DayList props, remove mock appointments data

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,62 +6,28 @@ import "components/Application.scss";
 
 import Appointment from "components/Appointment";
 
-//mock appointments data
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
 export default function Application(props) {
-
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-  const appointmentListItem = appointments.map(appointment => {
-    return (
-      <Appointment key={appointment.id} {...appointment} />
-    );
+  const [state, setState] = useState({ 
+    day: "Monday",
+    days: [],
+    appointments: []
   });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
     axios.get("/api/days").then(res => {
       setDays(res.data);
     });
   }, []);
-  
+
+  const appointmentListItem = state.appointments.map(appointment => {
+    return (
+      <Appointment key={appointment.id} {...appointment} />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -72,7 +38,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
