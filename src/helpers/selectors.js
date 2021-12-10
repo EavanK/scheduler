74b28fb21e8c3359225c736e12b5620e02ca051b
Hyunsu <ksoksl0711@gmail.com>
2021-12-10 00:12:25 -0800

    add helper function (getInterview)

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,19 @@
-export default function getAppointmentsForDay(state, day) {
+export const getAppointmentsForDay = (state, day) => {
   const {days, appointments} = state;
-
   let result = [];
-
   const filteredDay = days.find(filter => filter.name === day);
 
   if (filteredDay) {
     result = filteredDay.appointments.map(id => appointments[id])
   }
   return result;
+};
+
+export const getInterview = (state, interview) => {
+  if (!interview) return null;
+  
+  const { interviewers } = state;
+  const { interviewer } = interview;
+  interview.interviewer = interviewers[interviewer]
+  return interview;
 };
