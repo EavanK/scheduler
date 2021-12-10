42948efaa9a54cff381c2675a00d7ecf87e293cb
Hyunsu <ksoksl0711@gmail.com>
2021-12-10 00:14:41 -0800

    add interviewers obj into state, add helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import getAppointmentsForDay from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -11,7 +11,8 @@ export default function Application(props) {
   const [state, setState] = useState({ 
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   // change day state when click day
@@ -21,19 +22,30 @@ export default function Application(props) {
   useEffect(() => {
     const getDays = axios.get("/api/days");
     const getAppointments = axios.get("/api/appointments");
-    const getInterviews = axios.get("/api/interviewers");
+    const getInterviewers = axios.get("/api/interviewers");
     // get data at the same time (promise all)
-    Promise.all([getDays, getAppointments, getInterviews])
+    Promise.all([getDays, getAppointments, getInterviewers])
       .then(all => {
-        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }))
       });
   }, []);
 
   // use helper function to transform data that needs to pass down to Appointment component (getAppointmentsForDay)
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const appointmentListItem = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
-      <Appointment key={appointment.id} {...appointment} />
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+        interview={interview}
+      />
     );
   });
 
