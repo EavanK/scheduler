a7084f983f2b4b9259dbce3d05cc34de24e4c5ba
Hyunsu <ksoksl0711@gmail.com>
2021-12-12 16:33:44 -0800

    modify interviewer attribute in Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,29 +7,23 @@ import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
-
 export default function Appointment(props) {
-  const { id, time, interview, interviewers } = props;
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
+	const { id, time, interview, interviewers } = props;
+	const EMPTY = "EMPTY";
+	const SHOW = "SHOW";
+	const CREATE = "CREATE";
 
-  const { mode, transition, back } = useVisualMode(
-    interview ? SHOW : EMPTY
-  );
+	const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
-  return (  
-    <article className="appointment">
-      <Header time={time} />
-      {/* if you click button, add CRATE mode and change mode to CREATE mode */}
-      {mode === EMPTY &&  <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && (
-        <Show 
-          student={interview.student} 
-          interviewer={() => interview.interviewer} 
-        />
-      )}
-      {mode === CREATE && <Form interviewers={interviewers} onCancel={() => back()} />}
-    </article>
-  );
+	return (
+		<article className="appointment">
+			<Header time={time} />
+			{/* if you click button, add CRATE mode and change mode to CREATE mode */}
+			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+			{mode === SHOW && (
+				<Show student={interview.student} interviewer={interview.interviewer} />
+			)}
+			{mode === CREATE && <Form interviewers={interviewers} onCancel={() => back()} />}
+		</article>
+	);
 }
