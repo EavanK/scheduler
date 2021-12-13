0244d6acbfc3d9ffc5a956af4dc0e90008d45fb3
Hyunsu <ksoksl0711@gmail.com>
2021-12-13 15:40:21 -0800

    add save function, SAVE mode, Status component, send saving data to parent component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,27 +3,42 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
-
 import "components/Appointment/styles.scss";
 
-export default function Appointment(props) {
-	const { id, time, interview, interviewers } = props;
-	const EMPTY = "EMPTY";
-	const SHOW = "SHOW";
-	const CREATE = "CREATE";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVE = "SAVING";
 
+export default function Appointment(props) {
+	const { id, time, interview, interviewers, bookInterview } = props;
 	const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+	function save(name, interviewer) {
+		const interview = {
+			student: name,
+			interviewer,
+		};
+		transition(SAVE);
+		bookInterview(id, interview).then(() => {
+			transition(SHOW);
+		});
+	}
 
 	return (
 		<article className="appointment">
 			<Header time={time} />
 			{/* if you click button, add CRATE mode and change mode to CREATE mode */}
 			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+			{mode === SAVE && <Status message={SAVE} />}
 			{mode === SHOW && (
 				<Show student={interview.student} interviewer={interview.interviewer} />
 			)}
-			{mode === CREATE && <Form interviewers={interviewers} onCancel={() => back()} />}
+			{mode === CREATE && (
+				<Form interviewers={interviewers} onCancel={() => back()} onSave={save} />
+			)}
 		</article>
 	);
 }
