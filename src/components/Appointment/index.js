f37d4080828ab9b8a124ab124059a249b9ca330f
Hyunsu <ksoksl0711@gmail.com>
2021-12-13 17:14:43 -0800

    add confirm/onDelete function, Confirm Component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,18 +4,23 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
-const SAVE = "SAVING";
+const SAVE = "Saving";
+const DELETE = "Deleting";
+const CONFIRM = "CONFIRM";
+const confirmMessage = "Are you sure you would like to delete?";
 
 export default function Appointment(props) {
-	const { id, time, interview, interviewers, bookInterview } = props;
+	const { id, time, interview, interviewers, bookInterview, cancelInterview } = props;
 	const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
+	// create apopintment
 	function save(name, interviewer) {
 		const interview = {
 			student: name,
@@ -27,17 +32,39 @@ export default function Appointment(props) {
 		});
 	}
 
+	// send confirm message for deleting
+	function confirm() {
+		transition(CONFIRM);
+	}
+
+	// delete appointment
+	function onDelete() {
+		const interview = null;
+		transition(DELETE);
+		cancelInterview(id, interview).then(() => {
+			transition(EMPTY);
+		});
+	}
+
 	return (
 		<article className="appointment">
 			<Header time={time} />
 			{/* if you click button, add CRATE mode and change mode to CREATE mode */}
 			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
 			{mode === SAVE && <Status message={SAVE} />}
+			{mode === DELETE && <Status message={DELETE} />}
 			{mode === SHOW && (
-				<Show student={interview.student} interviewer={interview.interviewer} />
+				<Show
+					student={interview.student}
+					interviewer={interview.interviewer}
+					onDelete={() => confirm()}
+				/>
 			)}
 			{mode === CREATE && (
 				<Form interviewers={interviewers} onCancel={() => back()} onSave={save} />
+			)}
+			{mode === CONFIRM && (
+				<Confirm message={confirmMessage} onCancel={() => back()} onConfirm={onDelete} />
 			)}
 		</article>
 	);
