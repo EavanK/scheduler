97be693754fdb3c1bebdce692d6e7863ad81b657
Hyunsu <ksoksl0711@gmail.com>
2021-12-15 15:40:25 -0800

    add data-testid attribute in li element

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,17 +3,17 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const {name, spots, setDay, selected} = props;
-  const listClass = classNames("day-list__item", {
-    "day-list__item--selected": selected,
-    "day-list__item--full": (spots === 0)
-  });
-  return (
-    <li className={listClass} onClick={() => setDay(name)}>
-      <h2 className="text--regular">{name}</h2> 
-      {!spots && <h3 className="text--light">no spots remaining</h3>}
-      {spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
-      {spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
-    </li>
-  );
+	const { name, spots, setDay, selected } = props;
+	const listClass = classNames("day-list__item", {
+		"day-list__item--selected": selected,
+		"day-list__item--full": spots === 0,
+	});
+	return (
+		<li className={listClass} onClick={() => setDay(name)} data-testid="day">
+			<h2 className="text--regular">{name}</h2>
+			{!spots && <h3 className="text--light">no spots remaining</h3>}
+			{spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
+			{spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
+		</li>
+	);
 }
