190438e53cc9c59b3546ebb272910b59d4536c61
Hyunsu <ksoksl0711@gmail.com>
2021-12-16 02:58:46 -0800

    connect webSocket, get data from websocket server then dispatch

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -18,7 +18,7 @@ const initial = {
 };
 
 function reducer(state, action) {
-	const { day, days, appointments, interviewers, id, interview } = action.payload;
+	const { day, days, appointments, interviewers, id, interview } = action;
 	switch (action.type) {
 		case types.SET_DAY:
 			return { ...state, day };
@@ -38,8 +38,8 @@ function reducer(state, action) {
 export default function useApplicationData() {
 	const [state, dispatch] = useReducer(reducer, initial);
 
+	// get data from server api
 	useEffect(() => {
-		// get data from server api
 		Promise.all([
 			axios.get("/api/days"),
 			axios.get("/api/appointments"),
@@ -47,44 +47,38 @@ export default function useApplicationData() {
 		]).then(all => {
 			dispatch({
 				type: types.SET_APPLICATION_DATA,
-				payload: {
-					days: all[0].data,
-					appointments: all[1].data,
-					interviewers: all[2].data,
-				},
+				days: all[0].data,
+				appointments: all[1].data,
+				interviewers: all[2].data,
 			});
 		});
 	}, []);
 
-	// WebSocket
+	// connect webSocket
 	useEffect(() => {
-		const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-		// send message to server
-		webSocket.onopen = event => webSocket.send("ping");
+		const ws = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
 
-		// receive message from server
-		webSocket.onmessage = event => {
-			console.log(event.data);
+		// dispatch interview data from server
+		ws.onmessage = event => {
+			const data = JSON.parse(event.data);
+			data.type === types.SET_INTERVIEW && dispatch(data);
 		};
+		return () => ws.close();
 	}, []);
 
 	// change day state when click day
 	function setDay(day) {
-		dispatch({ type: types.SET_DAY, payload: { day } });
+		dispatch({ type: types.SET_DAY, day });
 	}
 
 	// book/edit appointment
 	function bookInterview(id, interview) {
-		return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
-			dispatch({ type: types.SET_INTERVIEW, payload: { id, interview } });
-		});
+		return axios.put(`/api/appointments/${id}`, { interview });
 	}
 
 	// cancel appointment
 	function cancelInterview(id) {
-		return axios.delete(`/api/appointments/${id}`).then(res => {
-			dispatch({ type: types.SET_INTERVIEW, payload: { id, interview: null } });
-		});
+		return axios.delete(`/api/appointments/${id}`);
 	}
 
 	return { state, setDay, bookInterview, cancelInterview };
