f55f4ec58b7276b4a3552683e16a36c9c51facfa
Hyunsu <ksoksl0711@gmail.com>
2021-12-16 01:55:35 -0800

    replace useState to useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,40 +1,66 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { setSpots } from "helpers/selectors";
 
-export default function useApplicationData() {
-	// combine states into one state(day, days, appointments)
-	const [state, setState] = useState({
-		day: "Monday",
-		days: [],
-		appointments: {},
-		interviewers: {},
-	});
+// action types
+const types = {
+	SET_DAY: "SET_DAY",
+	SET_APPLICATION_DATA: "SET_APPLICATION_DATA",
+	SET_INTERVIEW: "SET_INTERVIEW",
+};
 
-	// Get data from api server then setState
+// initial data
+const initial = {
+	day: "Monday",
+	days: [],
+	appointments: {},
+	interviewers: {},
+};
+
+function reducer(state, action) {
+	const { day, days, appointments, interviewers, id, interview } = action.payload;
+	switch (action.type) {
+		case types.SET_DAY:
+			return { ...state, day };
+		case types.SET_APPLICATION_DATA:
+			return { ...state, days, appointments, interviewers };
+		case types.SET_INTERVIEW: {
+			const appointment = { ...state.appointments[id], interview };
+			const appointments = { ...state.appointments, [id]: appointment };
+			const days = setSpots(state, appointments);
+			return { ...state, appointments, days };
+		}
+		default:
+			throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+	}
+}
+
+export default function useApplicationData() {
+	const [state, dispatch] = useReducer(reducer, initial);
+
 	useEffect(() => {
-		const getDays = axios.get("/api/days");
-		const getAppointments = axios.get("/api/appointments");
-		const getInterviewers = axios.get("/api/interviewers");
-		// get data at the same time (promise all)
-		Promise.all([getDays, getAppointments, getInterviewers]).then(all => {
-			setState(prev => ({
-				...prev,
-				days: all[0].data,
-				appointments: all[1].data,
-				interviewers: all[2].data,
-			}));
+		// get data from server api
+		Promise.all([
+			axios.get("/api/days"),
+			axios.get("/api/appointments"),
+			axios.get("/api/interviewers"),
+		]).then(all => {
+			dispatch({
+				type: types.SET_APPLICATION_DATA,
+				payload: {
+					days: all[0].data,
+					appointments: all[1].data,
+					interviewers: all[2].data,
+				},
+			});
 		});
 	}, []);
 
 	// WebSocket
 	useEffect(() => {
 		const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-
 		// send message to server
-		webSocket.onopen = event => {
-			webSocket.send("ping");
-		};
+		webSocket.onopen = event => webSocket.send("ping");
 
 		// receive message from server
 		webSocket.onmessage = event => {
@@ -43,39 +69,21 @@ export default function useApplicationData() {
 	}, []);
 
 	// change day state when click day
-	const setDay = day => setState({ ...state, day });
+	function setDay(day) {
+		dispatch({ type: types.SET_DAY, payload: { day } });
+	}
 
-	// create appointment
+	// book/edit appointment
 	function bookInterview(id, interview) {
-		const appointment = {
-			...state.appointments[id],
-			interview: { ...interview },
-		};
-		const appointments = {
-			...state.appointments,
-			[id]: appointment,
-		};
-
-		return axios.put(`/api/appointments/${id}`, { interview }).then(res => {
-			setState(prev => ({ ...prev, appointments }));
-			setState(prev => ({ ...prev, days: setSpots(prev) }));
+		return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+			dispatch({ type: types.SET_INTERVIEW, payload: { id, interview } });
 		});
 	}
 
-	// delete appointment
+	// cancel appointment
 	function cancelInterview(id) {
-		const appointment = {
-			...state.appointments[id],
-			interview: null,
-		};
-		const appointments = {
-			...state.appointments,
-			[id]: appointment,
-		};
-
 		return axios.delete(`/api/appointments/${id}`).then(res => {
-			setState(prev => ({ ...prev, appointments }));
-			setState(prev => ({ ...prev, days: setSpots(prev) }));
+			dispatch({ type: types.SET_INTERVIEW, payload: { id, interview: null } });
 		});
 	}
 
