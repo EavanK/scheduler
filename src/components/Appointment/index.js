bbe618994db966036b648555193862f6f5a549df
Hyunsu <ksoksl0711@gmail.com>
2021-12-16 02:56:43 -0800

    add useEffect to fix stale state bug (webSocket), regression test required

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
@@ -34,6 +34,12 @@ export default function Appointment(props) {
 	const { id, time, interview, interviewers, bookInterview, cancelInterview } = props;
 	const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
+	// fix stale state bug (for webSocket)
+	useEffect(() => {
+		interview && mode === EMPTY && transition(SHOW);
+		interview === null && mode === SHOW && transition(EMPTY);
+	}, [interview, transition, mode]);
+
 	// create apopintment
 	function save(name, interviewer) {
 		const interview = { student: name, interviewer };
@@ -58,7 +64,7 @@ export default function Appointment(props) {
 			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
 			{mode === SAVE && <Status message={SAVE} />}
 			{mode === DELETE && <Status message={DELETE} />}
-			{mode === SHOW && (
+			{mode === SHOW && interview && (
 				<Show
 					student={interview.student}
 					interviewer={interview.interviewer}
