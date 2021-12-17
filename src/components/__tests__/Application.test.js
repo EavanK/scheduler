8967809044237accb28d676419a54262da1b9f14
Hyunsu <ksoksl0711@gmail.com>
2021-12-16 22:24:01 -0800

    import mock axios function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 import { render, cleanup } from "@testing-library/react";
-
 import Application from "components/Application";
+import axios from "../../__mocks__/axios";
 
 afterEach(cleanup);
 
 it("renders without crashing", () => {
-  render(<Application />);
+	render(<Application />);
 });
