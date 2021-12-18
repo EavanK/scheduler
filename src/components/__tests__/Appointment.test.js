a47f44f2dee1c05eb28eddb72abc5f4711ca2d16
Hyunsu <ksoksl0711@gmail.com>
2021-12-18 01:49:05 -0800

    modify typo

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Application from "components/Application";
-import axios from "axios";
+import Appointment from "components/Appointment";
 
 describe("Appointment", () => {
 	it("renders without crashing", () => {
-		render(<Application />);
+		render(<Appointment />);
 	});
 });
