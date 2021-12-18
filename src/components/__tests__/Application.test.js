56dfb38f569dffc8f2a731af06b9fc1134599e22
Hyunsu <ksoksl0711@gmail.com>
2021-12-17 23:17:42 -0800

    add test queries for editing appointment and remaining sopts are the same

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -74,4 +74,30 @@ describe("Application", () => {
 
 		expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
 	});
+
+	// test editing appointment and remaining spots is still the same
+	it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+		const { container } = render(<Application />);
+
+		await waitForElement(() => getByText(container, "Archie Cohen"));
+
+		const appointment = getAllByTestId(container, "appointment").find(appointment =>
+			queryByText(appointment, "Archie Cohen"),
+		);
+
+		fireEvent.click(getByAltText(appointment, "Edit"));
+		fireEvent.change(getByTestId(appointment, "student-name-input"), {
+			target: { value: "Eavan Kim" },
+		});
+		fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+		fireEvent.click(getByText(appointment, "Save"));
+
+		expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+		await waitForElement(() => getByText(appointment, "Eavan Kim"));
+
+		const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+
+		expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+	});
 });
