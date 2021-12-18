883bd482d2244993bcbe50aff3a6f80844ad1f9c
Hyunsu <ksoksl0711@gmail.com>
2021-12-17 23:32:54 -0800

    add error handling test (saving)

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -12,6 +12,7 @@ import {
 	queryByText,
 } from "@testing-library/react";
 import Application from "components/Application";
+import axios from "axios";
 
 afterEach(cleanup);
 
@@ -98,6 +99,36 @@ describe("Application", () => {
 
 		const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
 
+		expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+	});
+
+	// error handleing test (saving)
+	it("shows the save error when failing to save an appointment", async () => {
+		axios.put.mockRejectedValueOnce();
+		const { container } = render(<Application />);
+
+		await waitForElement(() => getByText(container, "Archie Cohen"));
+
+		const appointment = getAllByTestId(container, "appointment").find(appointment =>
+			getByAltText(appointment, "Add"),
+		);
+
+		fireEvent.click(getByAltText(appointment, "Add"));
+		fireEvent.change(getByTestId(appointment, "student-name-input"), {
+			target: { value: "Lydia Miller-Jones" },
+		});
+		fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+		fireEvent.click(getByText(appointment, "Save"));
+
+		expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+		await waitForElement(() => getByText(appointment, "Could not save appointment"));
+
+		fireEvent.click(getByAltText(appointment, "Close"));
+
+		expect(getByText(appointment, "Save")).toBeInTheDocument();
+
+		const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
 		expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
 	});
 });
