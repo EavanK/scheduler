a9a8a900a8442ee6ee83ddea9f92aeec22065505
Hyunsu <ksoksl0711@gmail.com>
2021-12-19 01:51:44 -0800

    add format function to dry code

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,12 +8,17 @@ export default function DayListItem(props) {
 		"day-list__item--selected": selected,
 		"day-list__item--full": spots === 0,
 	});
+
+	function format(spots) {
+		if (!spots) return "no spots";
+		if (spots === 1) return "1 spot";
+		return `${spots} spots`;
+	}
+
 	return (
 		<li className={listClass} onClick={() => setDay(name)} data-testid="day">
 			<h2 className="text--regular">{name}</h2>
-			{!spots && <h3 className="text--light">no spots remaining</h3>}
-			{spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
-			{spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
+			<h3 className="text--light">{format(spots)} remaining</h3>
 		</li>
 	);
 }
